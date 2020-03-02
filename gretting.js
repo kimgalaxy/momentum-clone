adf90b1e2026d909a75d700f3a48926fac14cae6
kimgalaxy <55799302+kimgalaxy@users.noreply.github.com>
2020-03-03 06:16:46 +0900

    #3.3 Saving the user name part three

--- gretting.js
+++ gretting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "ls_key"; /* localStorage의 key와 연결이 되어있음 */
 SHOWING_CN = "showing"; /*showing은 css .showing과 연결되어 있음 */
 
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(ls_value) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
@@ -14,7 +30,7 @@ function paintGreeting(ls_value) {
 function loadName() {
   const key = localStorage.getItem(USER_LS); /** key는 결국 value값이다.*/
   if (key === null) {
-    // she is not
+    askForName();
   } else {
     paintGreeting(key);
   }
