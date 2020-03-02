12f0f767d6b4c9dc1f4eac11c6813506ab79577e
kimgalaxy <55799302+kimgalaxy@users.noreply.github.com>
2020-03-03 05:33:00 +0900

    3.2 Saving the user name part two

--- gretting.js
+++ gretting.js
@@ -1,28 +1,27 @@
-const form = document.querySelector(".js-form");
-const input = form.querySelector("input");
-const greeting = document.querySelector(".js-greetings");
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
+const USER_LS = "ls_key"; /* localStorage의 key와 연결이 되어있음 */
+SHOWING_CN = "showing"; /*showing은 css .showing과 연결되어 있음 */
 
-
-function paintGreeting(text) {
-    form.classList.remove(SHOWING_CN);
-    greeting.classList.add(SHOWING_CN);
-    greeting.innerHTML = `Hello ${text}`;
+function paintGreeting(ls_value) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${ls_value}`; /*text는 localStorage의 value이다.*/
 }
 
 function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if (currentUser === null) {
-
-    } else {
-        paintGreeting(currentUser);
-    }
+  const key = localStorage.getItem(USER_LS); /** key는 결국 value값이다.*/
+  if (key === null) {
+    // she is not
+  } else {
+    paintGreeting(key);
+  }
 }
 
 function init() {
-    loadName();
+  loadName();
 }
 
 init();
