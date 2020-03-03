962b6c53fb7ef24f11e0aa47a4a3e6653d64e627
kimgalaxy <55799302+kimgalaxy@users.noreply.github.com>
2020-03-04 03:29:21 +0900

    #4.2 Making a todo list part two

--- todo.js
+++ todo.js
@@ -4,15 +4,29 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 
 const TODOS_LS = "toDos"
 
+const toDos = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
     delBtn.innerText = "❌";
     const span = document.createElement("span");
-    span.innerText = text;
+    span.innerText = text; //매우 중요, 이것을 잘못 입력하면 엔터를 쳐도 입력이 안된다. 
+    const newId = toDos.length + 1;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
@@ -22,10 +36,15 @@ function handleSubmit(event) {
     toDoInput.value = "";
 }
 
-function loadTodos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
 
+
+function loadTodos() {
+    const loadedTodos = localStorage.getItem(TODOS_LS);
+    if (loadedTodos !== null) {
+        const parsedToDos = JSON.parse(loadedTodos);
+        parsedToDos.forEach(function (toDo) {
+            paintToDo(toDo.text);
+        });
     }
 }
 
